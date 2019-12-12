8d0703c5b9e384ebc7429f6f283b89de9e7dae37
mdapkus <dapkui.mantui@gmail.com>
2019-12-12 19:47:10 +0200

    Added function that gets you largest number.

--- GitTest/GitTest/Program.cs
+++ GitTest/GitTest/Program.cs
@@ -10,6 +10,16 @@ namespace GitTest
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Iveskite tris skaicius is eiles (spauskite enter po kiekvieno ivedimo)");
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+            int secondNumber = Convert.ToInt32(Console.ReadLine());
+            int thirdNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Largest number was {GetLargestNumber(firstNumber,secondNumber,thirdNumber)}.");
+        }
+        static int GetLargestNumber(int firstNumber,int secondNumber, int thirdNumber) {
+            int[] numbers = { firstNumber,secondNumber,thirdNumber};
+            int largestNumber = numbers.OrderByDescending(x => x).First();
+            return largestNumber;
         }
     }
 }
