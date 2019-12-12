6fa1e05a713bc77e938289884b5079cfdfacfc0a
mdapkus <dapkui.mantui@gmail.com>
2019-12-12 21:17:45 +0200

    Merge branch 'master' into FunctionPractice
    # Conflicts:
    #	GitTest/GitTest/Program.cs

--- GitTest/GitTest/Program_LOCAL_27852.cs
+++ GitTest/GitTest/Program_LOCAL_27852.cs
@@ -8,10 +8,8 @@ namespace GitTest
 {
     class Program
     {
-        static Random random = new Random();
         static void Main(string[] args)
         {
-<<<<<<< HEAD
             Console.WriteLine("Iveskite tris skaicius is eiles (spauskite enter po kiekvieno ivedimo)");
             int firstNumber = Convert.ToInt32(Console.ReadLine());
             int secondNumber = Convert.ToInt32(Console.ReadLine());
@@ -29,36 +27,6 @@ namespace GitTest
             int[] numbers = { firstNumber, secondNumber, thirdNumber };
             int middleNumber = numbers.OrderByDescending(x => x).ToArray()[1];
             return middleNumber == firstNumber;
-=======
-            Console.WriteLine("Insert number:");
-            int number = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine(IsPrimal(number));
-
-            for (int i = 0; i < 100; i++)
-            {
-                Console.WriteLine(D20());
-            }
-
-        }
-        static bool IsPrimal(int number)
-        {
-            if (number < 2)
-            {
-                return false;
-            }
-            for (int i = 2; i < number; i++)
-            {
-                if (number % i == 0)
-                {
-                    return false;
-                }
-            }
-            return true;
-        }
-        static int D20()
-        {
-            return random.Next(1, 20);
->>>>>>> master
         }
     }
 }
---
+++ GitTest/GitTest/Program_REMOTE_25924.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitTest
+{
+    class Program
+    {
+        static Random random = new Random();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insert number:");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(IsPrimal(number));
+
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine(D20());
+            }
+
+        }
+        static bool IsPrimal(int number)
+        {
+            if (number < 2)
+            {
+                return false;
+            }
+            for (int i = 2; i < number; i++)
+            {
+                if (number % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        static int D20()
+        {
+            return random.Next(1, 20);
+        }
+    }
+}
