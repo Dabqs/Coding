f995f5fc8df89a1a4ba0b0b6bda5c574e2266a65
mdapkus <dapkui.mantui@gmail.com>
2019-12-12 19:51:49 +0200

    Added another function that can tell if first number is within range of other two.

--- GitTest/GitTest/Program.cs
+++ GitTest/GitTest/Program.cs
@@ -15,11 +15,23 @@ namespace GitTest
             int secondNumber = Convert.ToInt32(Console.ReadLine());
             int thirdNumber = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine($"Largest number was {GetLargestNumber(firstNumber,secondNumber,thirdNumber)}.");
+            Console.WriteLine($"It is {InRange(firstNumber, secondNumber, thirdNumber)} that first number is in the middle of other two.");
         }
         static int GetLargestNumber(int firstNumber,int secondNumber, int thirdNumber) {
             int[] numbers = { firstNumber,secondNumber,thirdNumber};
             int largestNumber = numbers.OrderByDescending(x => x).First();
             return largestNumber;
+        }
+        static bool InRange(int firstNumber, int secondNumber, int thirdNumber)
+        {
+            bool firstNumberIsWithinRange = false;
+            int[] numbers = { firstNumber, secondNumber, thirdNumber };
+            int middleNumber = numbers.OrderByDescending(x => x).ToArray()[1];
+            if (middleNumber == firstNumber)
+            {
+                firstNumberIsWithinRange = true;
+            }
+            return firstNumberIsWithinRange;
         }
     }
 }
