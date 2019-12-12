35aed7b76c7d4cb9a26e5f839c609c265853a358
mdapkus <dapkui.mantui@gmail.com>
2019-12-12 20:40:47 +0200

    Added third function.

--- GitTest/GitTest/Program.cs
+++ GitTest/GitTest/Program.cs
@@ -10,6 +10,26 @@ namespace GitTest
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Insert number:");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(IsPrimal(number));
+        }
+        static bool IsPrimal(int number) {
+            bool isPrimal = true;
+            /*
+            if (number < 2)
+            {
+                return false;
+            }*/
+
+            for (int i = 2; i < number; i++)
+            {
+                if (number % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
         }
     }
 }
