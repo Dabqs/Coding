f84f270fa7b2e7b36d513e8519c342a7a4f7316a
mdapkus <dapkui.mantui@gmail.com>
2020-01-15 18:25:45 +0200

    Created project for function override lecture

---
+++ FunctionOverride_10/FunctionOverride_10/FunctionOverride_10/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionOverride_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
