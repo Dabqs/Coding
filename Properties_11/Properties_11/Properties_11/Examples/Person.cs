ddaa88a097124e4740ed9d4ee7d2046fb0689b8a
mdapkus <dapkui.mantui@gmail.com>
2020-01-15 21:28:27 +0200

    Played around with properties.

---
+++ Properties_11/Properties_11/Properties_11/Examples/Person.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Properties_11.Examples
+{
+    class Person
+    {
+        public string Name
+        {
+            get
+            {
+                Console.WriteLine("Getting name");
+                return name;
+            }
+            set
+            {
+                Console.WriteLine("Setting name");
+                name = value;
+            }
+        }
+        public string SurName
+        {
+            get
+            {
+                Console.WriteLine("Getting surname");
+                return surName;
+            }
+            //set { surName = value; }
+        }
+        public string Nationality
+        {
+            // get { return nationality; }
+            set
+            {
+                Console.WriteLine("Setting nationality");
+                nationality = value;
+            }
+        }
+        public string Religion
+        {
+            get
+            {
+                Console.WriteLine("Getting religion");
+                return religion;
+            }
+            private set
+            {
+                Console.WriteLine("Setting religion");
+                religion = value;
+            }
+        }
+
+        private string name = "no name";
+        private string surName = "No surname";
+        private string nationality = "no nationality";
+        private string religion = "no religion";
+
+
+
+
+        #region Examples
+        /*
+        public string jeans;
+
+        private string name = "No name";
+        //public string name { get; set; } = "no name"; //veikia tik jeigu vienoj eilut4je
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    name = value;
+                }
+            }
+        }
+        double age;
+        protected int income;
+        private double height;
+        public string surname { get; set; } = "No surname";
+        /*
+         private string surName = "No surname given";
+         public string Surname
+        {
+            get { return surName; }
+            set { surName = value; }
+        }
+
+        //public double height { get; set; }
+        public double Height
+        {
+            get
+            {
+                Console.WriteLine("Why do you want to know my height?");
+                return height;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    height = value;
+                }
+                else
+                {
+                    Console.WriteLine("Error. Height is negative");
+                }
+            }
+        }
+
+        public Person(string name, double age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+        public void IntroduceYourself()
+        {
+            Console.WriteLine($"My name is {name} and i'm {age} yo. My height is " + height);
+        }
+
+        public void SetAge(double newAge)
+        {
+            if (newAge > 0)
+            {
+                age = newAge;
+            }
+            else
+            {
+                Console.WriteLine("Age is negative");
+            }
+        }
+        public double GetAge()
+        {
+            return age;
+        }
+        */
+        #endregion
+    }
+}
--- Properties_11/Properties_11/Properties_11/Program.cs
+++ Properties_11/Properties_11/Properties_11/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Properties_11.Examples;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,27 @@ namespace Properties_11
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+            Person person = new Person();
+            Console.WriteLine(person.Name);
+            Console.WriteLine(person.SurName);
+          
+            // neveikia, nes nebera GET propertyje Console.WriteLine(person.Nationality); 
+            Console.WriteLine(person.Religion);
+            person.Name = "Mantas";
+           //nebeera SET property, d4l to negali priskirti reikšmės person.SurName= "Dapkus";
+            person.Nationality   = "Lithuanian";
+            // set yra private, dėl to keičiamas gali būti tik klasės viduje, o ne iš išorės person.Religion = "Catholic";
+            Console.WriteLine("Changed values-------------------");
+            Console.WriteLine(person.Name);
+            Console.WriteLine(person.SurName);
+            // neveikia, nes nebera GET propertyje Console.WriteLine(person.Nationality);
+            Console.WriteLine(person.Religion);
+
+
         }
+
     }
 }
