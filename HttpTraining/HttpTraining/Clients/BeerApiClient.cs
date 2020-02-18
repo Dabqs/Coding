74ad8da5b033f8d606a790addfa53db123499276
mdapkus <dapkui.mantui@gmail.com>
2020-02-18 18:05:00 +0200

    Commit for HTTP training.

---
+++ HttpTraining/HttpTraining/Clients/BeerApiClient.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpTraining.Clients
+{
+    class BeerApiClient
+    {
+        private const string beerApiUrl = @"http://ontariobeerapi.ca";
+
+        public List<Beer> GetAllBeers()
+        {
+            using HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(beerApiUrl);
+
+            using HttpResponseMessage response = client.GetAsync("products/").Result;
+            if (response.IsSuccessStatusCode)
+            {
+                string data = response.Content.ReadAsStringAsync().Result;
+                return JsonConvert.DeserializeObject<List<Beer>>(data);
+            }
+            else
+            {
+                Console.WriteLine("Something wronh happened: " + response.StatusCode);
+                return null;
+            }
+        }
+
+        internal void AddNewBeer(Beer beer) 
+        {
+            Console.WriteLine(JsonConvert.SerializeObject(beer));
+        }
+    }
+}
---
+++ HttpTraining/HttpTraining/Program.cs
@@ -0,0 +1,38 @@
+﻿using HttpTraining.Clients;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpTraining
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BeerApiClient beerApiClient = new BeerApiClient();
+            List<Beer> allBeers = beerApiClient.GetAllBeers();
+            if (allBeers != null)
+            {
+                var corona = from beer in allBeers
+                              where beer.Name.Contains("Corona")
+                              select beer;
+                corona.ToList().ForEach(Console.WriteLine);
+            }
+            else
+            {
+                Console.WriteLine("No beers found.");
+            }
+            beerApiClient.AddNewBeer(new Beer()
+            {
+                Name = "Svyturys",
+                Price = 2,
+                Country = "Lietuva",
+                OnSale = true,
+                Size = "Didelis"
+            });
+        }
+    }
+}
